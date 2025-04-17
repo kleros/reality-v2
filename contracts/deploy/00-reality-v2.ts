0177f0662fbbb05251b5367625142a85d10e4e81
jaybuidl <jb@kleros.io>
2025-04-17 20:05:13 +0100

    chore: bumped @kleros/kleros-v2-contracts, bumped hardhat

--- contracts/deploy/00-reality-v2.ts
+++ contracts/deploy/00-reality-v2.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HomeChains, isSkipped } from "./utils";
+import { DeploymentName, getContractsEthers } from "@kleros/kleros-v2-contracts";
 
 const disputeTemplateFn = (chainId: number, arbitratorAddress: string) => `{
     "title": "A reality.eth question",
@@ -33,8 +34,14 @@ const disputeTemplateFn = (chainId: number, arbitratorAddress: string) => `{
 const extraData =
   "0x00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000003";
 
+const NETWORK_TO_DEPLOYMENT: Record<string, DeploymentName> = {
+  arbitrumSepoliaDevnet: "devnet",
+  arbitrumSepolia: "testnet",
+  arbitrum: "mainnetNeo",
+} as const;
+
 const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { deployments, getNamedAccounts, getChainId } = hre;
+  const { deployments, getNamedAccounts, getChainId, ethers } = hre;
   const { deploy } = deployments;
 
   // fallback to hardhat node signers on local network
@@ -42,19 +49,26 @@ const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const chainId = Number(await getChainId());
   console.log("deploying to %s with deployer %s", HomeChains[chainId], deployer);
 
-  const klerosCore = await deployments.get("KlerosCore");
-  const disputeTemplateRegistry = await deployments.get("DisputeTemplateRegistry");
-  const disputeTemplate = disputeTemplateFn(chainId, klerosCore.address);
+  const networkName = deployments.getNetworkName();
+  const deploymentName = NETWORK_TO_DEPLOYMENT[networkName];
+
+  if (!deploymentName)
+    throw new Error(
+      `Unsupported network: ${networkName}. Supported networks: ${Object.keys(NETWORK_TO_DEPLOYMENT).join(", ")}`
+    );
+
+  const { klerosCore, disputeTemplateRegistry } = await getContractsEthers(ethers.provider, deploymentName);
+  const disputeTemplate = disputeTemplateFn(chainId, klerosCore.target as string);
   const disputeTemplateMappings = "TODO";
 
   await deploy("RealityV2", {
     from: deployer,
     args: [
-      klerosCore.address,
+      klerosCore.target,
       extraData,
       disputeTemplate,
       disputeTemplateMappings,
-      disputeTemplateRegistry.address,
+      disputeTemplateRegistry.target,
       600, // feeTimeout: 10 minutes
     ],
     log: true,
